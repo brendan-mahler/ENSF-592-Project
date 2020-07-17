6d4420d63be77b53460820b77476ec8a19de5542
brendan-mahler <63415688+brendan-mahler@users.noreply.github.com>
2020-07-17 06:50:41 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -4,18 +4,28 @@ import string
 
 client = pymongo.MongoClient('localhost', 27017)
 db = client['ENSF592_Project']
-location = db.Files
+locations = db.Files
+volumes = db.Files
 
-s = location.find_one({'year': 2017},{'the_geom'})
-s = s.get('the_geom')
+for i in range(1,100):
+    s = locations.find_one({'year': 2017},{'the_geom'})
+    s = s.get('the_geom')
+    s.strip()
+    strippables = string.ascii_letters + '()' + string.whitespace
+    s2 = s.strip(strippables)
+    coordinates_pairs = s2.split(',')
+    coordinates_list = []
+    for coordinates in coordinates_pairs:
+        coord_str = coordinates.split()
+        coordinates_list.append((float(coord_str[1]), float(coord_str[0])))
+    # print(coordinates_list)
 
 
-s.strip()
-strippables = string.ascii_letters + '()' + string.whitespace
-s2 = s.strip(strippables)
-coordinates_pairs = s2.split(',')
-coordinates_list = []
-for coordinates in coordinates_pairs:
-    coord_str = coordinates.split()
-    coordinates_list.append((float(coord_str[1]), float(coord_str[0])))
-print(coordinates_list)
+
+v = volumes.find({'year': 2017},{'volume'})
+sorted_v = []
+for volume in v:
+    sorted_v.append(volume["volume"])
+
+print(sorted(sorted_v, reverse=True))
+
